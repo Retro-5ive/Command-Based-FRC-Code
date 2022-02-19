09d68cb05658c79d5359675eb6bf9442ebb4e843
a_200408 <astridsanabria4@gmail.com>
2022-02-19 11:01:15 -0500

    first commit

---
+++ src/main/include/subsystems/Constants.h
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc2/command/CommandScheduler.h>
+
+/**
+ * The Constants header provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants.  This should not be used for any other
+ * purpose.
+ *
+ * It is generally a good idea to place constants into subsystem- or
+ * command-specific namespaces within this header, which can then be used where
+ * they are needed.
+ */
+
+#include <ctre/phoenix/motorcontrol/can/WPI_TalonSRX.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/SpeedControllerGroup.h>
+#include <frc/Joystick.h> 
+#include "rev/SparkMaxPIDController.h"
+#include <frc/motorcontrol/PWMSparkMax.h>
+#include <frc/Timer.h>
+
+class Constants : public::{
+
+
+
+};
+
